bcfbc09d5afc87e2a57cc7c768505eeff298c6cb
Evgenii <eugene.pokalyuk@gmail.com>
2024-02-05 02:41:21 +0700

    Add video avatar

--- src/components/ProfileCard/ProfileCard.tsx
+++ src/components/ProfileCard/ProfileCard.tsx
@@ -1,26 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useAppSelector } from '../../utils/hooks';
-// const VideoPlayer = () => {
-//     return (
-//         <div className='player-wrapper' style={{ width: '235px', height: '235px', borderRadius: '50%', overflow: 'hidden' }}>
-//         </div>
-//     );
-// };
 
 const ProfileCard: React.FC = () => {
     const tags = ["19 лет", "Москва", "", "Опыт 5 лет", "Выход 2 недели"];
-    const user = useAppSelector((store) => store.user)
+    const user = useAppSelector((store) => store.user);
+    const videoUrl = 'https://504818.selcdn.ru/MAX%20Helper%20Testimonials/Anya.mp4';
+    const [videoLoaded, setVideoLoaded] = useState(false);
+
     return (
-        <section className="flex flex-col items-center max-md:items-start max-md:justify-center text-black">
-            <div className='player-wrapper' style={{ width: '235px', height: '235px', borderRadius: '50%', overflow: 'hidden' }}>
-                <img
-                    className="rounded-full"
-                    src={user.stat.photo_url}
-                    // src={avatar}
-                    style={{ width: '100%', height: '100%' }}
-                />
+        <section className="flex flex-col items-center max-md:justify-center text-black">
+            <div className="video-container" style={{ width: '235px', height: '235px', borderRadius: '50%', overflow: 'hidden', position: 'relative' }}>
+                {!videoLoaded && (
+                    <img
+                        className="rounded-full"
+                        src={user.stat.photo_url}
+                        alt="Placeholder"
+                        style={{ width: '100%', height: '100%' }}
+                    />
+                )}
+                <video
+                    autoPlay
+                    loop
+                    muted
+                    style={{
+                        display: videoLoaded ? 'block' : 'none',
+                        position: 'absolute',
+                        width: '100%',
+                        height: '100%',
+                        objectFit: 'cover',
+                        transform: 'translate(-50%, -50%)',
+                        top: '50%',
+                        left: '50%'
+                    }}
+                    onLoadedData={() => setVideoLoaded(true)}
+                >
+                    <source src={videoUrl} type="video/mp4" />
+                    Your browser does not support the video tag.
+                </video>
             </div>
-            {/* <VideoPlayer /> */}
+
 
             <div className="text-center max-md:text-left mt-4">
                 <h1 className="text-[38px] font-[700] leading-[100%] -tracking-[1.5px] text-center">
