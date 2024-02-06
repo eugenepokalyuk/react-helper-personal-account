79a081f6df18a6a011750b0d5abf15d9e6be3e30
Evgenii <eugene.pokalyuk@gmail.com>
2024-02-06 20:18:34 +0700

    Update changes

--- src/components/SkillsSection/SkillsSection.tsx
+++ src/components/SkillsSection/SkillsSection.tsx
@@ -58,9 +58,14 @@ const SkillsSection: React.FC = () => {
         return <IconBad className={iconClass} />;
     };
 
+    const getBackgroundColor = (averageRate: number) => {
+        if (averageRate >= 7) return "#44C894";
+        if (averageRate > 5 && averageRate < 7) return "#FFB84B";
+        return "#E94230";
+    };
+
     const selectedCategory = skills.find((category: Category) => category.name === selectedSkill);
     const averageRateForSelectedCategory = selectedCategory ? calculateAverageRateForCategory(selectedCategory) : 0;
-
     return (
         <article className="mt-6 pb-[50px]">
             <div className="flex px-2 mb-[40px] max-md:flex-col">
@@ -78,24 +83,14 @@ const SkillsSection: React.FC = () => {
 
             {selectedCategory.skills.map((skill: any, index: number) => {
                 const reviewsAverageRate = calculateReviewsAverageRate(skill.reviews);
+                // const textColor = getBackgroundColor(reviewsAverageRate); // Get the background color
+                const bgColor = getBackgroundColor(reviewsAverageRate); // Get the background color
                 return (
                     <div
                         key={index}
-                        className="relative p-[11px] flex flex-col md:flex-row items-start mb-12 last:mb-0 hover:bg-[#F6F6F6] hover:cursor-pointer rounded-[16px] active:bg-[#F1F1F1]"
+                        className="relative py-4 px-5 flex flex-col md:flex-row items-start mb-[2rem] last:mb-0 hover:bg-[#F6F6F6] hover:cursor-pointer rounded-[16px] active:bg-[#F1F1F1]"
                         onClick={() => handleCardClick(index)}
                     >
-                        {/* <div className='md:hidden flex justify-between w-full items-center mb-4 md:mb-0'>
-                            <div className="flex w-full md:w-1/3 text-left items-center">
-                                <div className='w-[27px] h-[27px] max-md:w-[33px] max-md:h-[33px] mr-1'>
-                                    {selectIconByAverageRateFull(reviewsAverageRate)}
-                                </div>
-                                <h4 className="text-[#1E1E1E] text-[20px] font-[500] leading-[115%]">{skill.name}</h4>
-                            </div>
-                            <div>
-                                <IconAccord className='fill-black' />
-                            </div>
-                        </div> */}
-
                         <div className="flex w-full md:w-1/3 text-left items-center mb-4 md:mb-0 justify-between">
                             <div className='flex items-center'>
                                 <div className='w-[27px] h-[27px] max-md:w-[33px] max-md:h-[33px] mr-1'>
@@ -108,8 +103,6 @@ const SkillsSection: React.FC = () => {
                                 <IconAccord className='fill-[#1E1E1E]/30' />
                             </div>
                         </div>
-
-
 
                         <div className="w-full md:w-2/3 relative">
                             <p className="text-[16px] text-[#1E1E1E] font-[400] leading-[115%] max-w-[395px]">Здесь мы объяняем насколько учитывает это при работе Знает что его решения влияют на некоторые метрики, учитывает это при работе</p>
@@ -124,22 +117,37 @@ const SkillsSection: React.FC = () => {
                             <>
                                 <div className="max-md:hidden expanded-card absolute top-[29%] left-[50%] -translate-x-[50%] translate-y-[29%] w-[135%] bg-white card-shadows rounded-[36px] z-10">
                                     <div>
-                                        {selectedCategory.skills[expandedCardIndex].reviews.map((review: Review, userIndex: number) => (
-                                            <div key={userIndex} className={`grid grid-cols-3 items-center mx-[30px] py-[20px] ${userIndex !== 0 && 'border-t'}`}>
-                                                <UserCard key={userIndex} user={review} isModal={true} />
-                                                <div className='flex'>
-                                                    <div className='w-[27px] h-[27px] max-md:w-[33px] max-md:h-[33px] mr-1'>
-                                                        {selectIconByAverageRateFull(review.rate)}
+                                        {selectedCategory.skills[expandedCardIndex].reviews.map((review: Review, userIndex: number) => {
+                                            const textColor = getBackgroundColor(review.rate); // Get the background color
+                                            return (
+                                                <div key={userIndex} className={`grid grid-cols-3 items-center mx-[30px] py-[20px] ${userIndex !== 0 && 'border-t'}`}>
+                                                    <UserCard key={userIndex} user={review} isModal={true} />
+                                                    <div className='flex'>
+                                                        <div className='w-[27px] h-[27px] max-md:w-[33px] max-md:h-[33px] mr-1'>
+                                                            {selectIconByAverageRateFull(review.rate)}
+                                                        </div>
+                                                        <p
+                                                            className='text-[#4FCC9B] text-[25px] font-[500] leading-[105%]'
+                                                            style={{
+                                                                color: textColor
+                                                            }}
+                                                        >{review.rate}/10</p>
                                                     </div>
-                                                    <p className='text-[#4FCC9B] text-[25px] font-[500] leading-[105%]'>{review.rate}/10</p>
+                                                    <div>
+                                                        <p className='text-[#1E1E1E] text-[13px] font-[400] leading-[115%]'>Здесь мы объяняем насколько учитывает это при работе. Знает что его решения влияют на некоторые метрики, учитывает это при работе.</p>
+                                                    </div>
                                                 </div>
-                                                <div>
-                                                    <p className='text-[#1E1E1E] text-[13px] font-[400] leading-[115%]'>Здесь мы объяняем насколько учитывает это при работе. Знает что его решения влияют на некоторые метрики, учитывает это при работе.</p>
-                                                </div>
-                                            </div>
-                                        ))}
+                                            )
+                                        }
+
+                                        )}
                                     </div>
-                                    <div className='bg-[#44C894] rounded-b-[36px] grid grid-cols-3 items-center px-[30px] py-[15px]'>
+                                    <div
+                                        className={`bg-[#44C894] rounded-b-[36px] grid grid-cols-3 items-center px-[30px] py-[15px]`}
+                                        style={{
+                                            background: bgColor
+                                        }}
+                                    >
                                         <h2 className='text-[#fff] text-[21.5px] font-[400] leading-[115%]'>Средняя оценка</h2>
                                         <h1 className='text-[#fff] text-[39.5px] font-[400] leading-[105%]'>{calculateAverageRate(selectedCategory.skills[expandedCardIndex].reviews)}/10</h1>
                                         <p className='text-[#fff] text-[13px] font-[400] leading-[115%]'>Здесь мы объяняем насколько учитывает это при работе. Знает что его решения влияют на некоторые метрики, учитывает это при работе.</p>
@@ -167,26 +175,41 @@ const SkillsSection: React.FC = () => {
 
                                     <div className='px-5 mt-[30px]'>
                                         <h1 className='text-[24px] text-[#000] font-[700] leading-[105%] -tracking-[-0.5px]'>Оценки от специалистов</h1>
-                                        {selectedCategory.skills[expandedCardIndex].reviews.map((review: Review, userIndex: number) => (
-                                            <div key={userIndex} className={`grid grid-cols-1 items-center py-5 space-y-[14px] ${userIndex !== 0 && 'border-t'}`}>
-                                                <div className='flex justify-between items-center'>
-                                                    <UserCard key={userIndex} user={review} isModal={true} />
-                                                    <div className='flex'>
-                                                        <div className='w-[27px] h-[27px] max-md:w-[33px] max-md:h-[33px] mr-1'>
-                                                            {selectIconByAverageRateFull(review.rate)}
+                                        {selectedCategory.skills[expandedCardIndex].reviews.map((review: Review, userIndex: number) => {
+                                            const textColor = getBackgroundColor(review.rate); // Get the background color
+                                            return (
+                                                <div key={userIndex} className={`grid grid-cols-1 items-center py-5 space-y-[14px] ${userIndex !== 0 && 'border-t'}`}>
+                                                    <div className='flex justify-between items-center'>
+                                                        <UserCard key={userIndex} user={review} isModal={true} />
+                                                        <div className='flex'>
+                                                            <div className='w-[27px] h-[27px] max-md:w-[33px] max-md:h-[33px] mr-1'>
+                                                                {selectIconByAverageRateFull(review.rate)}
+                                                            </div>
+                                                            <p
+                                                                className='text-[#4FCC9B] text-[25px] font-[500] leading-[105%]'
+                                                                style={{
+                                                                    color: textColor
+                                                                }}
+                                                            >{review.rate}/10</p>
                                                         </div>
-                                                        <p className='text-[#4FCC9B] text-[25px] font-[500] leading-[105%]'>{review.rate}/10</p>
+                                                    </div>
+
+                                                    <div>
+                                                        <p className='text-[#1E1E1E] text-[16px] font-[400] leading-[115%]'>Здесь мы объяняем насколько учитывает это при работе. Знает что его решения влияют на некоторые метрики, учитывает это при работе.</p>
                                                     </div>
                                                 </div>
+                                            )
+                                        }
 
-                                                <div>
-                                                    <p className='text-[#1E1E1E] text-[16px] font-[400] leading-[115%]'>Здесь мы объяняем насколько учитывает это при работе. Знает что его решения влияют на некоторые метрики, учитывает это при работе.</p>
-                                                </div>
-                                            </div>
-                                        ))}
+                                        )}
                                     </div>
 
-                                    <div className='bg-[#44C894] grid grid-cols-1 items-center px-[30px] py-[15px] space-y-[14px]'>
+                                    <div
+                                        className={`bg-[#44C894] grid grid-cols-1 items-center px-[30px] py-[15px] space-y-[14px]`}
+                                        style={{
+                                            background: bgColor
+                                        }}
+                                    >
                                         <h2 className='text-[#fff] text-[21.5px] font-[400] leading-[115%]'>Средняя оценка</h2>
                                         <h1 className='text-[#fff] text-[39.5px] font-[400] leading-[105%]'>{calculateAverageRate(selectedCategory.skills[expandedCardIndex].reviews)}/10</h1>
                                         <p className='text-[#fff] text-[13px] font-[400] leading-[115%]'>Здесь мы объяняем насколько учитывает это при работе. Знает что его решения влияют на некоторые метрики, учитывает это при работе.</p>
--- src/components/UserCard/UserCard.tsx
+++ src/components/UserCard/UserCard.tsx
@@ -22,7 +22,6 @@ const UserCard: React.FC<UserCardProps> = ({ user, isModal }) => {
                 <div className="relative mr-2">
                     {!isModal && selectIconByAverageRate(user.rate, 18)}
                     <div className={`${isModal ? 'w-[58px] h-[58px]' : 'w-[32px] h-[32px]'}`}>
-                        {/* <div className={`${isModal ? 'w-[58px] h-[58px]' : 'w-[32px] h-[32px]'}`}> */}
                         <img src={user.avatarUrl} alt={user.name} className="rounded-full object-cover" />
                     </div>
                     <div>
@@ -35,27 +34,6 @@ const UserCard: React.FC<UserCardProps> = ({ user, isModal }) => {
                     <p className={`${!isModal ? 'truncate max-w-[80px]' : ''} text-[12px] text-[#000]/45 font-[400] leading-[110%] mt-1`}>{user.position}</p>
                 </div>
             </div>
-            {/* <div className={`flex items-center`}>
-                <div className="relative mr-2">
-                    {!isModal && user.rate >= 7.5 ? (
-                        <IconGood className='w-[18px] h-[18px] absolute -left-1.5 -top-1.5' />
-                    ) : !isModal && (user.rate > 5 && user.rate < 7.4) ? (
-                        <IconNormal className='w-[18px] h-[18px] absolute -left-1.5 -top-1.5' />
-                    ) : !isModal && user.rate <= 5 && (
-                        <IconBad className='w-[18px] h-[18px] absolute -left-1.5 -top-1.5' />
-                    )}
-                    <div className={`${isModal ? 'w-[58px] h-[58px]' : 'w-[32px] h-[32px]'}`}>
-                        <img src={user.avatarUrl || Avatar} alt={user.name} className="rounded-full object-cover" />
-                    </div>
-                    <div>
-                        <img src={user.bottomRightIcon} className={`${isModal ? 'w-[23px] h-[23px]' : 'w-[15px] h-[15px]'} text-red-500 absolute -bottom-1 -right-1 rounded-full`} />
-                    </div>
-                </div>
-                <div>
-                    <h2 className={`${isModal ? 'text-[17px] font-[500]' : 'text-[12px] font-[400]'} text-[#000] leading-[105%]`}>{user.name}</h2>
-                    <p className={`${!isModal ? 'truncate max-w-[80px]' : ''} text-[12px] text-[#000]/45 font-[400] leading-[110%] mt-1`}>{user.position}</p>
-                </div>
-            </div> */}
         </>
     );
 };
