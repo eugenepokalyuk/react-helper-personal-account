2f0d82319ee7813a668e40b9dbaf9019bdadf0ee
Evgenii <eugene.pokalyuk@gmail.com>
2024-02-06 20:41:27 +0700

    Update SkillsSection.tsx

--- src/components/SkillsSection/SkillsSection.tsx
+++ src/components/SkillsSection/SkillsSection.tsx
@@ -87,7 +87,7 @@ const SkillsSection: React.FC = () => {
                 return (
                     <div
                         key={index}
-                        className="relative py-4 px-5 flex flex-col md:flex-row items-start mb-[2rem] last:mb-0 hover:bg-[#F6F6F6] hover:cursor-pointer rounded-[16px] active:bg-[#F1F1F1]"
+                        className="relative py-4 px-5 flex flex-col md:flex-row items-start mb-[1rem] last:mb-0 hover:bg-[#F6F6F6] hover:cursor-pointer rounded-[16px] active:bg-[#F1F1F1]"
                         onClick={() => handleCardClick(index)}
                     >
                         <div className="flex w-full md:w-1/3 text-left items-center mb-4 md:mb-0 justify-between">
